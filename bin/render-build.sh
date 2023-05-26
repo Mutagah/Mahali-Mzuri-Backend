ef00deb31c62212a756a7973f0cbd3d5268e8629
Mutaga Kihahu <kihahumutagah@gmail.com>
2023-05-26 21:36:58 +0300

    Milestone : Edited the render build script to reset db on production

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,5 +4,5 @@ set -o errexit
 
 bundle install
 RAILS_ENV=production rake db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
-bundle exec rake db:migrate
-bundle exec rake db:seed
+# bundle exec rake db:migrate
+# bundle exec rake db:seed
